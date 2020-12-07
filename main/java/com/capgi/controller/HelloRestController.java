8dd648ef81e74616596c8a898cab74b257037b55
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-12-07 20:34:22 +0530

    put request

--- main/java/com/capgi/controller/HelloRestController.java
+++ main/java/com/capgi/controller/HelloRestController.java
@@ -3,6 +3,7 @@ package com.capgi.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -32,6 +33,11 @@ public class HelloRestController {
 	@PostMapping("/post")
 	public String sayHello(@RequestBody User user) {
 		return "Hello " + user.getfName() + " " + user.getlName() + "!";
+	}
+
+	@PutMapping("/put/{fName}")
+	public String sayHelloPut(@PathVariable String fName, @RequestParam(value = "lName") String lName) {
+		return "Hello " + fName + " " + lName + "!";
 	}
 
 }
