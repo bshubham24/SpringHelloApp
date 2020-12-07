8080ffc35e5babeb28b0fcf61401ac16a7c86fd5
bshubham24 <acer@DESKTOP-QLPFQS8>
2020-12-07 19:28:54 +0530

    get request

--- main/java/com/capgi/controller/HelloRestController.java
+++ main/java/com/capgi/controller/HelloRestController.java
@@ -4,8 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloRestController {
-	@RequestMapping("/hello")
+	@RequestMapping(value = {"", "/", "/home"})
 	public String sayHello() {
 		return "Hello from Shubham";
 	}
